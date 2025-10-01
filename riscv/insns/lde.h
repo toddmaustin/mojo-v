dce374ecc97b6263b60683433ccf6a0d673fde97
Todd Austin <austin@umich.edu>
2025-10-01 13:29:01 -0400

    Added non-encrypted versions of lde/sde instructions.

--- riscv/insns/lde.h
+++ riscv/insns/lde.h
@@ -1,4 +1,12 @@
 require_extension(EXT_ZKMOJOV);
 
+// Mojo-V: LDE requires SECREGs enabled AND RD must be secret reg
+if (!p->get_secreg_mode() || !IS_SECREG(insn.rd()))
+{
+  // illegal use of LDE
+  throw trap_illegal_instruction(insn.bits());
+}
+
+// Mojo-V: read 3rd-party decrypted value into SECREG RD
 WRITE_RD(MMU.load<int64_t>(BASE_RS1 + insn.i_imm()));
 
--- riscv/insns/sde.h
+++ riscv/insns/sde.h
@@ -1,4 +1,13 @@
 require_extension(EXT_ZKMOJOV);
 
+// Mojo-V: SDE requires SECREGs enabled AND RS2 must be secret reg
+if (!p->get_secreg_mode() || !IS_SECREG(insn.rs2())) 
+{
+  // illegal use of SDE
+  throw trap_illegal_instruction(insn.bits());
+}
+
+// Mojo-V: store 3rd-party encrypted value from SECREG RS2
+
 MMU.store<uint64_t>(BASE_RS1 + insn.s_imm(), RS2);
 
