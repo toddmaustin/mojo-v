faa11c6899da2ee7bda3cdef3034cb507423c7d9
Todd Austin <austin@umich.edu>
2026-04-21 12:25:37 +0000

    Added *highlife* automoton simulator.

--- bringup-bench/highlife/highlife.cc
+++ bringup-bench/highlife/highlife.cc
@@ -10,9 +10,9 @@
 
 namespace {
 
-constexpr unsigned kRows = 16;
-constexpr unsigned kCols = 16;
-constexpr unsigned kIterations = 1000;
+constexpr unsigned kRows = 20;
+constexpr unsigned kCols = 20;
+constexpr unsigned kIterations = 250;
 
 uint64e_t board_a[kRows][kCols];
 uint64e_t board_b[kRows][kCols];
@@ -27,10 +27,27 @@ inline unsigned wrap_col(int c) {
   return static_cast<unsigned>((c + static_cast<int>(kCols)) % static_cast<int>(kCols));
 }
 
+void print_board(unsigned i, board_t& board) {
+  libmin_printf("HighLife %ux%u after %u iterations:\n", kRows, kCols, i);
+
+  unsigned alive_count = 0;
+  for (unsigned r = 0; r < kRows; ++r) {
+    for (unsigned c = 0; c < kCols; ++c) {
+      const uint64_t cell = board[r][c].decrypt();
+      const char ch = cell ? '#' : '.';
+      libmin_printf("%c", ch);
+      alive_count += static_cast<unsigned>(cell);
+    }
+    libmin_printf("\n");
+  }
+
+  libmin_printf("Alive cells: %u\n", alive_count);
+}
+
 void init_board() {
   for (unsigned r = 0; r < kRows; ++r) {
     for (unsigned c = 0; c < kCols; ++c) {
-      const uint64_t v = (r * 37u + c * 73u + (r ^ c) * 19u + 11u) & 1u;
+      const uint64_t v = libmin_rand() & 1u;
       board_a[r][c] = v;
       board_b[r][c] = 0;
     }
@@ -56,7 +73,7 @@ void step_highlife(board_t& cur, board_t& nxt) {
       const uint64e_t alive = cur[r][c];
       const uint64e_t n = neighbor_count(cur, r, c);
       const uint64e_t survive = (n == 2) | (n == 3);
-      const uint64e_t born = (n == 3) | (n == 6);
+      const uint64e_t born = (n == 3) | /* extra highlife birth condition */(n == 6);
       nxt[r][c] = cmov(alive, survive, born);
     }
   }
@@ -67,6 +84,8 @@ board_t& run_simulation() {
   board_t* nxt = &board_b;
 
   for (unsigned i = 0; i < kIterations; ++i) {
+    if ((i % 25) == 0)
+      print_board(i, *cur);
     step_highlife(*cur, *nxt);
     board_t* tmp = cur;
     cur = nxt;
@@ -74,23 +93,6 @@ board_t& run_simulation() {
   }
 
   return *cur;
-}
-
-void print_board(board_t& board) {
-  libmin_printf("HighLife %ux%u after %u iterations:\n", kRows, kCols, kIterations);
-
-  unsigned alive_count = 0;
-  for (unsigned r = 0; r < kRows; ++r) {
-    for (unsigned c = 0; c < kCols; ++c) {
-      const uint64_t cell = board[r][c].decrypt();
-      const char ch = cell ? '#' : '.';
-      libmin_printf("%c", ch);
-      alive_count += static_cast<unsigned>(cell);
-    }
-    libmin_printf("\n");
-  }
-
-  libmin_printf("Alive cells: %u\n", alive_count);
 }
 
 }  // namespace
@@ -105,9 +107,12 @@ int main(void) {
   if (debug_context(SIMON128_KEY, CONTRACT_SIG) != 0)
     return -1;
 
+  // initialize RNG
+  libmin_srand(42);
+
   init_board();
   board_t& final_board = run_simulation();
-  print_board(final_board);
+  print_board(kIterations, final_board);
 
   libmin_success();
   return 0;
